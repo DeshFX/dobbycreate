174ef85a62fbc308c56c604c2398e0bd7865f06c
DeshFX <twitsyahrul@gmail.com>
2025-08-16 21:36:00 +0700

    app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ load_dotenv()
 
 app = Flask(__name__)
 
-app.secret_key = os.getenv('SECRET_KEY', secrets.token_hex(24))
+app.secret_key = secrets.token_hex(24)
 app.permanent_session_lifetime = timedelta(hours=2)
 
 limiter = Limiter(
